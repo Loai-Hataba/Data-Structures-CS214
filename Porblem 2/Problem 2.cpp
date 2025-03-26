ccbae953870cfe80d705fa210d55fc0ab9219af8
Ahmed Abdelaziz <162904583+HosStdeez@users.noreply.github.com>
2025-03-27 01:54:36 +0200

    display poly func is done

--- Porblem 2/Problem 2.cpp
+++ Porblem 2/Problem 2.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
 using namespace std;
 
-//void displayPoly(int* poly, int size){
-//    bool first = true;
-//    for (int i = size-1; i >= 0; --i) {
-//        if (poly[i] != 0) {
-//            if (!first) {
-//                if (poly[i] > 0) {
-//                    cout << " + ";
-//                }
-//                else if (poly[i] < 0) cout << " - ";
-//            }
-//            else if(poly[i] < 0)cout<<'-';
-//
-//            if(i == 0){
-//                cout<<" = "<<poly[i];
-//            }
-//            else if (abs(poly[i] > 1)){
-//                cout<<abs(poly[i])<<'x';
-//            }
-//            else cout<< 'x';
-//
-//            if(i - 2 > 1)cout<<'^'<<i-2;
-//
-//        }
-//        first = false;
-//    }
-//    if (first) cout<<"0";
-//    cout<<endl;
-//}
+void displayPoly(int* poly, int size){
+    bool first = true;
+    bool beforeLast = false;
+    int deg = size - 2;
+    int lhsIsEmpty = true;
+    for (int i = size-1; i >= 0; --i) {
+        if (i == 0) {
+            if (lhsIsEmpty){
+                cout<<'0';
+            }
+            cout << " = " << poly[i];
+            break;
+        }
+        if (poly[i] != 0) {
+            if (!first) {
+                if (poly[i] > 0) {
+                    cout << " + ";
+                } else if (poly[i] < 0) cout << " - ";
+            } else if (poly[i] < 0)cout << '-';
+            if (beforeLast){
+                cout<<abs(poly[i]);
+                continue;
+            }
+            else if(abs(poly[i]) > 1){
+                cout<<abs(poly[i]);
+            }
+            if (deg > 1){
+                cout<<"x^"<<deg;
+                lhsIsEmpty = false;
+            }
+            else {
+                cout<<'x';
+                lhsIsEmpty = false;
+            }
+            first = false;
+        }
+        if (deg == 1 ){
+            beforeLast = true;
+        }
+        deg--;
+    }
+    cout << endl;
+}
+
+
 
 
 int main() {
@@ -50,13 +67,13 @@ int main() {
     int* poly2 = new int[sz2];
 
     cout<<"Enter polynomial: ";
-    for (int i = 0; i < sz1; ++i) {
+    for (int i = 0; i < sz2; ++i) {
         cin>>poly2[i];
     }
 
-//    cout<<"first polynomial: ";
-//    displayPoly(poly1, sz1);
-//    cout<<"second polynomial: ";
-//    displayPoly(poly2, sz2);
+    cout<<"first polynomial: ";
+    displayPoly(poly1, sz1);
+    cout<<"second polynomial: ";
+    displayPoly(poly2, sz2);
 
 }
