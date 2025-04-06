12c4f126ef150fb9150984bea62f0075a99db505
Ahmed Abdelaziz <162904583+HosStdeez@users.noreply.github.com>
2025-04-06 14:21:50 +0200

    file input & some test cases on prob 2

--- Porblem 2/Problem 2.cpp
+++ Porblem 2/Problem 2.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
-void displayPoly(int* poly, int size){
+void displayPoly(int *poly, int size) {
     bool first = true;
     bool beforeLast = false;
     int deg = size - 2;
     int lhsIsEmpty = true;
-    for (int i = size-1; i >= 0; --i) {
+    for (int i = size - 1; i >= 0; --i) {
         if (i == 0) {
-            if (lhsIsEmpty){
-                cout<<'0';
+            if (lhsIsEmpty) {
+                cout << '0';
             }
             cout << " = " << poly[i];
             break;
@@ -20,24 +22,22 @@ void displayPoly(int* poly, int size){
                     cout << " + ";
                 } else if (poly[i] < 0) cout << " - ";
             } else if (poly[i] < 0)cout << '-';
-            if (beforeLast){
-                cout<<abs(poly[i]);
+            if (beforeLast) {
+                cout << abs(poly[i]);
                 continue;
+            } else if (abs(poly[i]) > 1) {
+                cout << abs(poly[i]);
             }
-            else if(abs(poly[i]) > 1){
-                cout<<abs(poly[i]);
-            }
-            if (deg > 1){
-                cout<<"x^"<<deg;
+            if (deg > 1) {
+                cout << "x^" << deg;
                 lhsIsEmpty = false;
-            }
-            else {
-                cout<<'x';
+            } else {
+                cout << 'x';
                 lhsIsEmpty = false;
             }
             first = false;
         }
-        if (deg == 1 ){
+        if (deg == 1) {
             beforeLast = true;
         }
         deg--;
@@ -45,73 +45,71 @@ void displayPoly(int* poly, int size){
     cout << endl;
 }
 
-int* sum(int* poly1, int*poly2, int* res, int size1, int size2, int maxSize){
+int *sum(const int *poly1,const int *poly2, int *res, int size1, int size2, int maxSize) {
     for (int i = 0; i < maxSize; ++i) {
-        if (i >= size1){
+        if (i >= size1) {
             res[i] = poly2[i];
-        }
-        else if (i >= size2){
+        } else if (i >= size2) {
             res[i] = poly1[i];
-        }
-        else res[i] = poly1[i] + poly2[i];
-    }
-    return res;
-}
-int* diff(int* poly1, int* poly2, int *res, int size1, int size2, int maxSize){
-    for (int i = 0; i < maxSize; ++i) {
-        if (i >= size1){
-            res[i] = poly2[i];
-        }
-        else if (i >= size2){
-            res[i] = poly1[i];
-        }
-        else res[i] = poly2[i] - poly1[i];
+        } else res[i] = poly1[i] + poly2[i];
     }
     return res;
 }
 
-
+int *diff(const int *poly1, const int *poly2, int *res, int size1, int size2, int maxSize) {
+    for (int i = 0; i < maxSize; ++i) {
+        if (i >= size1) {
+            res[i] = poly2[i];
+        } else if (i >= size2) {
+            res[i] = poly1[i];
+        } else res[i] = poly2[i] - poly1[i];
+    }
+    return res;
+}
 
 
 int main() {
+    ifstream infile("test_5.txt");              //                <----------------- specify test file from here cuh
+    if (!infile.is_open()) {
+        cout << "Error opening input.txt\n";
+        return 1;
+    }
+
     int deg1, deg2;
 
-    cout<<"Order of first polynomial: ";
-    cin>>deg1;
-    int sz1 = deg1+2;
-    int* poly1 = new int[sz1];
+    infile >> deg1;
+    int sz1 = deg1 + 2;
+    int *poly1 = new int[sz1];
 
-    cout<<"Enter polynomial: ";
     for (int i = 0; i < sz1; ++i) {
-        cin>>poly1[i];
+        infile >> poly1[i];
     }
 
-    cout<<"Order of second polynomial: ";
-    cin>>deg2;
-    int sz2 = deg2+2;
-    int* poly2 = new int[sz2];
+    infile >> deg2;
+    int sz2 = deg2 + 2;
+    int *poly2 = new int[sz2];
 
-    int maxSZ = max(sz1,sz2);
-
-    cout<<"Enter polynomial: ";
     for (int i = 0; i < sz2; ++i) {
-        cin>>poly2[i];
+        infile >> poly2[i];
     }
 
-    cout<<"first polynomial: ";
+    infile.close();
+
+    int maxSZ = max(sz1, sz2);
+
+    cout << "First polynomial: ";
     displayPoly(poly1, sz1);
-    cout<<"second polynomial: ";
+    cout << "Second polynomial: ";
     displayPoly(poly2, sz2);
 
-
-    int* sumPoly = new int[maxSZ];
+    int *sumPoly = new int[maxSZ];
     sumPoly = sum(poly1, poly2, sumPoly, sz1, sz2, maxSZ);
-    cout<<"Sum of the two polynomials: ";
+    cout << "Sum of polynomials: ";
     displayPoly(sumPoly, maxSZ);
 
-    int* diffPoly = new int[maxSZ];
+    int *diffPoly = new int[maxSZ];
     diffPoly = diff(poly1, poly2, diffPoly, sz1, sz2, maxSZ);
-    cout<<"Difference of the two polynomials: ";
+    cout << "Difference of polynomials: ";
     displayPoly(diffPoly, maxSZ);
 
     delete[] poly1;
