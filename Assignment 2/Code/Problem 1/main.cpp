9614ee73e394a92c0d730ce459090893978f622e
Abdallah Mohamed <160781861+Abdallah229@users.noreply.github.com>
2025-05-04 20:18:03 +0300

    setting enviroment for assignment 2

---
+++ Assignment 2/Code/Problem 1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ;
+
+// 1 making the stack dataStructure using dynamic array allocation
+
+// 2  get the input from file
+
+// 3 implementing the go forward function
+
+// 4 Implementing the go back function
+
+// 5 making test cases
+
+int main() {
+
+
+    return 0 ;
+}
