ab0e31df9cf8c926b6bc54bb7cc0b2dc5aea2cbd
Abdelaziz28 <162904583+HosStdeez@users.noreply.github.com>
2025-05-05 22:39:25 +0300

    setting problem 3

---
+++ Assignment 2/Code/Problem 3/cmake-build-debug/CMakeFiles/Problem_3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Problem_3.dir/main.cpp.obj"
+  "CMakeFiles/Problem_3.dir/main.cpp.obj.d"
+  "Problem_3.exe"
+  "Problem_3.exe.manifest"
+  "Problem_3.pdb"
+  "libProblem_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Problem_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
