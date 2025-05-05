47ce149fa20328ecd7cbbe7d3f75d7bd4ec38421
Abdelaziz28 <162904583+HosStdeez@users.noreply.github.com>
2025-05-06 01:36:57 +0300

    prob 3 done but O(n*k) might work on that

--- Assignment 2/Code/Problem 3/main.cpp
+++ Assignment 2/Code/Problem 3/main.cpp
@@ -15,37 +15,72 @@ int main(){
     getline(infile, line1); //only first line
 
     stringstream ss(line1);
-    int k;
-    int arr[100];
-    int i = 0;              //size
-    while(ss >> arr[i]){
-        i++;
+    int capacity = 4;
+    int size = 0;
+    int* arr = new int[capacity];
+
+    int x;
+    while (ss >> x) {
+        if (size == capacity) {
+            capacity *= 2;
+            int* newArr = new int[capacity];
+
+            for (int i = 0; i < size; ++i) {
+                newArr[i] = arr[i];
+            }
+
+            delete[] arr;
+            arr = newArr;
+        }
+
+        arr[size++] = x;
     }
+
+    int k;
     infile>>k;
 
-    int cntToK = 0;
+//    int cntToK = 0;
     int cntOfSub = 0;
-    bool done = false;
-    bool inArow = false;    //zeros in a row
-    for (int j = 0; j < i; ++j) {
-        if (arr[j] == 0 && cntToK < k){         // count zeros to k
-            cntToK++;
-            done = true;
-            inArow = true;
-        }
-        if (arr[j] == 0 && cntToK == k){
-            cntToK = 0;
+    bool done = true;
+//    bool inArow = false;    //subarr begins
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] == 0){
+            for (int j = 0; j < k; ++j) {
+                if (i+j == size){
+                    done = false;
+                    break;
+                }
+                arr[i+j] = !arr[i+j];
+            }
             cntOfSub++;
-            done = true;
-            inArow = false;
         }
-        if(arr[j] == 1 && cntToK <= k && inArow) {
-            done = false;
-            break;
-        }
+        else continue;
+//        if (inArow){
+//            arr[j] =!arr[j];
+//            cntToK++;
+//        }
+//        else if(arr[j] == 1){
+//            continue;
+//        }
+//        if (cntToK > k){
+//            cntToK = 0;
+//        }
+//        if (arr[j] == 0 && cntToK < k){         // count zeros to k
+//            cntToK++;
+//            inArow = true;
+//        }
+//        if (arr[j] == 0 && cntToK == k){
+//            cntToK = 0;
+//            cntOfSub++;
+//            inArow = false;
+//        }
+//        if(arr[j] == 1 && cntToK <= k && inArow) {
+//            done = false;
+//            break;
+//        }
     }
 
-    if (!done || cntToK > 0){
+    if (!done){
         cout<<"-1"<<endl;
     }
     else {
